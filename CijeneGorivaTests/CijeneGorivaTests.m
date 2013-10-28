c12d9bf8863778759388a240f2a3e2e5a27a14ed
Ivor Prebeg <Ivor.Prebeg@gmail.com>
2013-10-29 00:08:06 +0100

    added tests

---
+++ CijeneGorivaTests/CijeneGorivaTests.m
@@ -0,0 +1,34 @@
+//
+//  CijeneGorivaTests.m
+//  CijeneGorivaTests
+//
+//  Created by Ivor Prebeg on 29/10/13.
+//  Copyright (c) 2013 Ivor Prebeg. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CijeneGorivaTests : XCTestCase
+
+@end
+
+@implementation CijeneGorivaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
